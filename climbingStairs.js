d3e68124c68e462db39fae7f8fdaf8a9ad984abf
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-06-17 17:56:26 -0400

    Add files via upload

---
+++ climbingStairs.js
@@ -0,0 +1,22 @@
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+var climbStairs = function(n, memo = {}) {
+    if(n in memo) return memo[n]
+    if(n < 0) return 0
+    if(n === 0) return 1
+
+    let sum = 0
+
+    for(let i = 1; i <=2; i++){
+        sum += climbStairs(n - i, memo)
+    }
+
+    memo[n] = sum
+    return memo[n]
+};
+
+console.log(climbStairs(5))
