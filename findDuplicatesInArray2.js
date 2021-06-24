29de8d1abab03aa251de73b89246afd93421e07c
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-06-24 15:43:47 -0400

    Create findDuplicatesInArray2.js

---
+++ findDuplicatesInArray2.js
@@ -0,0 +1,20 @@
+/*
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, 
+return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+*/
+
+var findDuplicates = function(nums) {
+    let result = []
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[Math.abs(nums[i])-1] > 0){
+           nums[Math.abs(nums[i])-1] = -nums[Math.abs(nums[i])-1]
+        }else{
+            result.push(Math.abs(nums[i]))
+        }
+    }
+    
+    return result
+};
