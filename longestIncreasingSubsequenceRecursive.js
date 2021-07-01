c190e714cff2778b9b45729722c1caa535b5ef61
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-07-01 10:49:10 -0400

    Create longestIncreasingSubsequenceRecursive.js

---
+++ longestIncreasingSubsequenceRecursive.js
@@ -0,0 +1,52 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+*/
+
+
+var lengthOfLIS = function(nums) {
+    
+    let max = 1
+    let memo = {}
+    
+    memo[0] = 1
+    
+    for(let i = 1; i < nums.length; i++){
+        max = Math.max(max, longestTillHere(i, nums, memo))
+    }
+    
+    return max
+};
+
+function longestTillHere(index, nums, memo){
+        if(index in memo){
+            return memo[index]
+        }
+        
+        let max = 1
+        
+        for(let j = 0; j < index; j++){
+            if(nums[j] < nums[index]){
+                max = Math.max(max, longestTillHere(j, nums, memo)+1)
+            }
+        }
+        memo[index] = max
+        return max
+}
