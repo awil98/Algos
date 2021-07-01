3ac3fd07fb491addcc1741db374c6ca61646d86d
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-07-01 13:05:04 -0400

    Update longestPalindromicSubstring.js

--- longestPalindromicSubstring.js
+++ longestPalindromicSubstring.js
@@ -1,3 +1,24 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+
+*/
+
+
+
 var longestPalindrome = function(s) {
    
     let longest = ""
