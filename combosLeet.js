08ac0197b505567ee57823a59e114ae19611c2ad
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-07-01 21:22:21 -0400

    Update combosLeet.js

--- combosLeet.js
+++ combosLeet.js
@@ -2,6 +2,17 @@
 Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
 
 You may return the answer in any order.
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
 */
 
 var combine = function(n, k) {
