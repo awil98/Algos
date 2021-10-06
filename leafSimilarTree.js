fcffa1995e78ea886e2bc73470f2b68edd67c180
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-10-06 13:22:24 -0700

    Create leafSimilarTree.js

---
+++ leafSimilarTree.js
@@ -0,0 +1,90 @@
+/*
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+Input: root1 = [1], root2 = [1]
+Output: true
+Example 3:
+
+Input: root1 = [1], root2 = [2]
+Output: false
+Example 4:
+
+Input: root1 = [1,2], root2 = [2,2]
+Output: true
+Example 5:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    let t1 = []
+    let t2 = []
+    
+    traversal(root1, t1)
+    traversal(root2, t2)
+    
+    if(t1.length != t2.length){
+        return false
+    }
+    
+    for(let i = 0; i < t1.length; i++){
+        if(t1[i] != t2[i]){
+            return false
+        }
+    }
+    
+    return true
+};
+
+function traversal(root, arr){
+    if(root.left === null && root.right === null){
+       arr.push(root.val)
+       return
+    }
+    
+    if(root.left != null){
+        traversal(root.left, arr)
+    }
+    
+    if(root.right != null){
+        traversal(root.right, arr)
+    }
+}
